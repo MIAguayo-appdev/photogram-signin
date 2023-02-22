3f5f0fc8243aabdc798185eb25a7cd61d5c56da0
MIAguayo <maguayo@chicagobooth.edu>
2023-02-22 17:40:52 +0000

    project done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,37 @@
 class CommentsController < ApplicationController
 
+  def index
+  comments = Comment.all.order({ :created_at => :asc })
+
+  render({ :json => comments.as_json})
+  end
+
+  def show
+    the_id = params.fetch(:the_comment_id)
+    comment = Comment.where({ :id => the_id}).at(0)
+
+    render({ :json => comments.as_json})
+  end
 
   def create
     comment = Comment.new
 
-    comment.author_id = params.fetch("input_author_id")
+    comment.author_id = session.fetch(:user_id)
     comment.photo_id = params.fetch("input_photo_id")
     comment.body = params.fetch("input_body")
 
     comment.save
 
-    redirect_to("/photos/#{comment.photo_id}")
+    respond_to do |format|
+      format.json do
+        render({ :json => comment.as_json})
+      end
+
+      format.html do
+        redirect_to("/photos/#{comment.photo_id}")
+      end
+    end
+
   end
 
 

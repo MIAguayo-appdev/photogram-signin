169e4b8e4f3e50baffdc717b8aa6cce621dc2545
MIAguayo <maguayo@chicagobooth.edu>
2023-02-22 04:48:51 +0000

    sign in sign out and sign up working properly 60%

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,47 @@
 class UsersController < ApplicationController
   
+  def authenticate
+    #get the username
+    un = params.fetch("input_username")
+    #get the password
+    pw = params.fetch("input_password")
+
+    #look up the record from the db matching username
+    user = User.where({ :username => un}).at(0)
+
+    # if there's no record, redirect back to sign in form
+    if user == nil 
+     redirect_to("/user_sign_in", { :alert => "username nonexistent" })
+    else
+      #if there is a record, check to see if password matches
+      #if so, set the cookie 
+      #redirect to homepage
+      if user.authenticate(pw) #this returns the record, any non nil counts as true
+        session.store(:user_id, user.id)
+        redirect_to("/", { :notice => "Welcome back " + user.username + "!" })
+      else
+        #if password does not match, redirect back to sign in form
+        redirect_to("/user_sign_in", { :alert => "password does not match" })
+      end
+    end
+  end
+
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!" })
+  end
+
   def new_registration_form
 
     render({ :template => "users/signup_form.html.erb" })
   end
+
+  def new_session_form
+
+    render({ :template => "users/signin_form.html.erb" })
+  end
+
   
   def index
     @users = User.all.order({ :username => :asc })
@@ -28,6 +66,8 @@ class UsersController < ApplicationController
     save_status = user.save
 
     if save_status == true
+      session.store(:user_id, user.id) #to sign in 
+
       redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!"})
     else
       redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
